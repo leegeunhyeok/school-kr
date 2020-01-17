328eb5021b45a51ec8c167ab4ac7d70908b2ed92
leegeunhyeok <dev.ghlee@gmail.com>
2020-01-18 02:20:23 +0900

    ADD: test code

---
+++ test.js
@@ -0,0 +1,8 @@
+const School = require('./dist/index')
+const school = new School()
+school.search(School.Region.GYEONGGI, '광명경영회계고')
+  .then(result => {
+    const code = result[0].schoolCode
+    school.init(School.Type.HIGH, School.Region.GYEONGGI, code)
+    school.getMeal().then(console.log)
+  })
