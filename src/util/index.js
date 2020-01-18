090e9202629fe6da55cd38ac3c5c9d816f8ff257
leegeunhyeok <dev.ghlee@gmail.com>
2020-01-18 11:03:52 +0900

    FIX: util.js

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,8 @@
+/**
+ * 대상 숫자의 길이가 length가 되도록 앞 자리에 0을 채워 반환
+ * @param {number} number 자릿수를 맞출 대상 수
+ * @param {number} length 0을 채워 length 길이로 만들기
+ */
 const paddingNumber = (number, length) => {
   if (length === undefined) {
     return number.toString();
@@ -5,7 +10,7 @@ const paddingNumber = (number, length) => {
 
   const targetNumber = number.toString();
   let p = '';
-  for (let i = 0; i < targetNumber.length - length; i++) {
+  for (let i = 0; i < length - targetNumber.length; i++) {
     p += '0';
   }
 
