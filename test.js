e3d9fee66de2cfe65f917b4eda2b2e939100beaa
leegeunhyeok <dev.ghlee@gmail.com>
2020-01-18 17:29:21 +0900

    REMOVE: test code

--- test.js
+++
@@ -1,8 +0,0 @@
-const School = require('./dist/entry')
-const school = new School()
-school.search(School.Region.GYEONGGI, '광명경영회계고')
-  .then(result => {
-    const code = result[0].schoolCode
-    school.init(School.Type.HIGH, School.Region.GYEONGGI, code)
-    school.getMeal().then(console.log)
-  })
